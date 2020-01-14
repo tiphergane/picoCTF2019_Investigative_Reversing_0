6c4b6d6869d2c088c00e365fe8361c0365763680
tiphergane <tiphergane@gmail.com>
2020-01-14 08:10:16 +0100

    Shorter is better
    Thx to XC from work for the help with online converters.

--- PicoCTF_Investigative_Reversing_0/Program.cs
+++ PicoCTF_Investigative_Reversing_0/Program.cs
@@ -12,25 +12,14 @@ namespace PicoCTF_Investigative_Reversing_0
                 0x70, 0x69, 0x63, 0x6F, 0x43, 0x54, 0x4B, 0X80, 0x6B, 0x35, 0x7A, 0x73, 0x69, 
                 0x64, 0x36, 0x71, 0x5F, 0x35, 0x37, 0x64, 0x30, 0x64, 0x34, 0x37, 0x63, 0x7D 
             };
-
-            //BaseFlag = Encoding.ASCII.GetBytes("picoCTKçk5zsid6q_57d0d47c}"); 
-            int i;
             for (i = 6; i < 0xf; i++)
-            {
-                BaseFlag[i] -= 0x5;
-            }
-            //BaseFlag[7] = 0x7b; // to fix the problem who take the wrong ASCII char
-            BaseFlag[i] += 0x3;
-            
-            /* is this bloc really usefull ? I don't think so. We will try tonight after work
-            for (int j = 0x10; j < 0x1a; j++)
-            {
-                BaseFlag[j] = BaseFlag[j];
-            }
-            */
-            string s = Encoding.UTF8.GetString(BaseFlag);
-            Console.WriteLine("Flag is : {0}", s);
-            Console.WriteLine("Press Enter to quit");
+                {
+                    BaseFlag[i] -= 0x5;
+                }
+			BaseFlag[i] += 0x3;
+			string s =  Encoding.UTF8.GetString(BaseFlag);;
+			Console.WriteLine("Flag is : {0}", s);
+			Console.WriteLine("Press Enter to quit");
             while (Console.ReadKey().Key != ConsoleKey.Enter) ;
         }
     }
